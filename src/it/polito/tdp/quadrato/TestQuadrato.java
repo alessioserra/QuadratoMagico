429283c6b6d42b68a0382856f8268c3af87abf10
zierp <zierp@172.22.43.134>
2019-04-08 12:05:07 +0200

    versione non ottimizzata

---
+++ src/it/polito/tdp/quadrato/TestQuadrato.java
@@ -0,0 +1,14 @@
+package it.polito.tdp.quadrato;
+
+public class TestQuadrato {
+
+	public static void main(String[] args) {
+
+		CercaQuadrato cq = new CercaQuadrato() ;
+		
+		cq.genera(3);
+		
+		System.out.println( cq.soluzioni ) ;
+	}
+
+}
